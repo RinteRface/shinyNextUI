739dfe869088b18ef21367abfc88a7aa6ebab028
DivadNojnarg <dgranjon@ymail.com>
2023-11-10 08:10:30 +0100

    will fix modal issue

--- R/feedback.R
+++ R/feedback.R
@@ -4,17 +4,18 @@
 modal <- component("Modal")
 
 #' @rdname modal
-#' @inherit component params return
+#' @export
+modal_content <- component("ModalContent")
+
+#' @rdname modal
 #' @export
 modal_header <- component("ModalHeader")
 
 #' @rdname modal
-#' @inherit component params return
 #' @export
 modal_body <- component("ModalBody")
 
 #' @rdname modal
-#' @inherit component params return
 #' @export
 modal_footer <- component("ModalFooter")
 
@@ -24,16 +25,13 @@ modal_footer <- component("ModalFooter")
 popover <- component("Popover")
 
 #' @rdname popover
-#' @inherit component params return
 #' @export
 popover_trigger <- component("PopoverTrigger")
 
 #' @rdname popover
-#' @inherit component params return
 #' @export
 popover_content <- component("PopoverContent")
 
 #' @rdname tooltip
-#' @inherit component params return
 #' @export
 tooltip <- component("Tooltip")
